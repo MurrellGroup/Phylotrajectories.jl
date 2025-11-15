b663f09a6c92fc5c07d50091a2cb9925b229df93
Mariia Guryleva <mariia.guryleva@ki.se>
2025-11-15 07:30:45 +0100

    Phylotrajectories.jl: Added export of OU_MCMC_tree_inference

--- src/Phylotrajectories.jl
+++ src/Phylotrajectories.jl
@@ -1,7 +1,7 @@
 module Phylotrajectories
 
 using CSV,
-    DataFrames, MolecularEvolution, StatsBase, Distributions, Phylo, Plots, LinearAlgebra, ForwardBackward
+    DataFrames, MolecularEvolution, StatsBase, Distributions, Phylo, Plots, LinearAlgebra, ForwardBackward, SpecialFunctions
 
 include("inference/inference.jl")
 include("importing.jl")
@@ -10,7 +10,7 @@ include("recombination.jl")
 
 include("viz/plotting.jl")
 
-export tree_inference, import_count_matrix, sim_count_matrix, recombine
+export OU_MCMC_tree_inference, tree_inference, import_count_matrix, sim_count_matrix, recombine
 export IndependentBrownianMotion, IndependentGaussiansPartition, FBGaussianPartition, OrnsteinUhlenbeckModel
 export FrequencySampler, RootAcceptanceRatio, GaussianStateSample, MeanDriftSampler, ContinuousUpdate
 export OUVarianceSampler, OUContinuousUpdate, OUGaussianStateSample, OUThetaSampler, OUEqmuSampler
