9a1f315ac66c36d93880e8e24abece9e062e148c
Mariia Guryleva <mariia.guryleva@ki.se>
2026-03-18 23:31:54 +0100

    LogExponential distribution export was added

--- src/Phylotrajectories.jl
+++ src/Phylotrajectories.jl
@@ -1,7 +1,7 @@
 module Phylotrajectories
 
 using CSV,
-    DataFrames, MolecularEvolution, StatsBase, Distributions, Phylo, Plots, LinearAlgebra, ForwardBackward, SpecialFunctions
+    DataFrames, MolecularEvolution, StatsBase, Distributions, Phylo, Plots, LinearAlgebra, ForwardBackward, SpecialFunctions, Statistics
 
 include("inference/inference.jl")
 include("importing.jl")
@@ -9,13 +9,15 @@ include("simulations.jl")
 include("recombination.jl")
 
 include("viz/plotting.jl")
+include("utils/utils.jl")
 
 export tree_inference, import_count_matrix, sim_count_matrix, recombine
-export IndependentBrownianMotion, IndependentGaussiansPartition, FBGaussianPartition, OrnsteinUhlenbeckModel
+export IndependentBrownianMotion, IndependentGaussiansPartition, FBGaussianPartition, OrnsteinUhlenbeckModel, LogExponential
 export FrequencySampler, RootAcceptanceRatio, GaussianStateSample, MeanDriftSampler, ContinuousUpdate
 export OUVarianceSampler, OUContinuousUpdate, OUGaussianStateSample, OUThetaSampler, OUEqmuSampler
 export DiscreteModel, ContinuousModel, OUContinuousModel
 
 export PlotTreeOnUmap, PlotTreeOnUmapNoAnimShadow, PlotTreeOnUmapNoAnim
+export run_ou_and_build_clone_matrix, SimComparison
 
 end
